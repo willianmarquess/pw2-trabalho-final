4b63d2ab1c6fadbc41d303aa6226c58f12e60633
Willian Marques <willian.goncalves4@gmail.com>
2025-10-16 18:56:33 -0300

    Make 'nome' column unique in 'categoria' table

--- tema2.sql
+++ tema2.sql
@@ -8,10 +8,8 @@ CREATE TABLE usuario (
 
 CREATE TABLE categoria (
     id_categoria SERIAL PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
-    descricao VARCHAR(150),
-    id_usuario INT NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario) ON DELETE CASCADE
+    nome VARCHAR(50) NOT NULL UNIQUE,
+    descricao VARCHAR(150)
 );
 
 CREATE TABLE despesa (
